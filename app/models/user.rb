d6c2df7a62f12d34d4a11c328e2a5f2b38e96d75
andre-tlima <andre-tlima@hotmail.com>
2019-03-30 14:09:36 +0000

    teste

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,18 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+    
+    validates_uniqueness_of :auth_token
+    before_create :generate_authentication_token!
+    #has_many :gains, dependent :destroy
+    
+    def info
+        "#{email} - #{created_at} - Token: #{Devise.friendly_toke}"
+    end
+    
+    def generate_authentication_token!
+        begin
+            self.auth_token = Devise.friendly_token
+        end while User.exists(auth_token: auth_token)
+    end
 end
